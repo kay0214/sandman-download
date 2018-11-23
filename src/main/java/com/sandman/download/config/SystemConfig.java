7ad68736506c9f80b11059fe1279a2382479b709
sunpeikai <spk123456>
2018-11-23 17:00:03 +0800

    添加全局异常处理

--- src/main/java/com/sandman/download/config/SystemConfig.java
+++ src/main/java/com/sandman/download/config/SystemConfig.java
@@ -3,9 +3,13 @@
  */
 package com.sandman.download.config;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @author sunpeikai
  * @version SystemConfig, v0.1 2018/11/23 11:07
  */
+@Component
 public class SystemConfig {
+
 }
