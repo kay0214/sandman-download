44f5c24b8df2cf044ab9e4b22f4fe82ff7ae109f
sunpeikai <spk123456>
2018-12-21 16:45:27 +0800

    恶魔喵

--- emmmoe/src/main/java/com/sandman/emmmoe/EmmmoeApplication.java
+++ emmmoe/src/main/java/com/sandman/emmmoe/EmmmoeApplication.java
@@ -2,15 +2,14 @@ package com.sandman.emmmoe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class EmmmoeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EmmmoeApplication.class, args);
     }
-
 }
 
